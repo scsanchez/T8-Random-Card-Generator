7acd98208e5385de2f6428bd7fe82468b05bfbff
scsanchez <sergiocarrascal000@gmail.com>
2021-04-24 21:58:44 +0000

    first upload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,54 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  const number = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let suit = ["diamonds", "hearts", "clubs", "spades"];
+  // let suit = [
+  //     "https://cdn1.iconfinder.com/data/icons/games-7/48/Diamonds-512.png",
+  //     "https://cdn1.iconfinder.com/data/icons/games-7/48/Diamonds-512.png",
+  //     "https://cdn1.iconfinder.com/data/icons/games-7/48/Diamonds-512.png",
+  //     "https://cdn1.iconfinder.com/data/icons/games-7/48/Diamonds-512.png"
+  // ];
+  //   let suit = ["❤", "✌", "🤷‍♂️", "🐱‍🏍"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let a = randomNumber(number);
+  let b = randomSuit(suit);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#number").innerHTML = randomNumber(number);
 };
+
+function randomNumber(number) {
+  let randomnumber = Math.floor(Math.random() * number.length);
+  let showNumber = number[randomnumber];
+  console.log(showNumber);
+  return showNumber;
+}
+
+function randomSuit(suit) {
+  let randomsuit = Math.floor(Math.random() * suit.length);
+  let showSuit = suit[randomsuit];
+
+  // document.querySelectorAll(".symbol").forEach(element => {
+  //     element.classList.add(showSuit)
+  // });
+
+  const nodes = document.querySelectorAll(".symbol");
+  for (i = 0; i < nodes.length; i++) {
+    nodes[i].classList.add(showSuit);
+  }
+
+  console.log(showSuit);
+  return showSuit;
+}
